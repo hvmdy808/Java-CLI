b2afc2a9b53b0cb7401dfff27ff9c39caac01374
hvmdy808 <hamdymody901@gmail.com>
2025-10-15 14:43:05 +0300

    Parser

--- src/Parser.java
+++ src/Parser.java
@@ -1,3 +1,67 @@
-public class Parser {
+import java.util.Arrays;
 
+public class Parser {
+    private String commandName;
+    private String[] args = new String[4];
+//    public Parser(String commandName, String[] args) {
+//        this.commandName = commandName;
+//        this.args = args;
+//    }
+    public void parse(String input) {
+        String [] arr = input.split(" ");
+        int i = 1;
+        commandName = arr[0];
+        if (commandName.equals("cp") && arr[1].equals("-r")) {
+            commandName+=' '+arr[1];
+            i = 2;
+        }
+        int j = 0;
+        for (; i < arr.length; i++) {
+            args[j] = arr[i];
+            j++;
+        }
+    }
+    public String getCommandName() {
+        return commandName;
+    }
+    public String[] getArgs() {
+        return args;
+    }
 }
+
+
+
+
+
+//    public void parse(String input) {
+//        StringBuilder name = new StringBuilder();
+//        int i = 0;
+//        for (i=0; i<input.length(); i++) {
+//            char c = input.charAt(i);
+//            if(c != ' ') {
+//                name.append(c);
+//            }else{
+//                i++;
+//                break;
+//            }
+//        }
+//        if(name.equals("cp")){
+//            StringBuilder word = new StringBuilder();
+//            for (; i < input.length(); i++) {
+//                char c = input.charAt(i);
+//                if(c != ' ') {
+//                    word.append(c);
+//                }else{
+////                    i++;
+//                    break;
+//                }
+//            }
+//            if(word.toString().equals("-r")){
+//                name.append(' ').append(word.toString());
+//            }else{
+//                args[0] = word.toString();
+//            }
+//            commandName = name.toString();
+//        }
+//
+//    }
--- src/Terminal.java
+++ src/Terminal.java
@@ -1,3 +1,68 @@
-public class Terminal {
+import java.util.Scanner;
 
+public class Terminal {
+    static Parser parser = new Parser();
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        while (true){
+            String input = scanner.nextLine();
+            parser.parse(input);
+            if(parser.getCommandName().equals("pwd")){
+                System.out.println("lol1");
+            } else if(parser.getCommandName().equals("cd")){
+                System.out.println("lol2");
+            } else if(parser.getCommandName().equals("ls")){
+                System.out.println("lol3");
+            } else if(parser.getCommandName().equals("mkdir")){
+                System.out.println("lol4");
+            } else if(parser.getCommandName().equals("rmdir")){
+                System.out.println("lol5");
+            } else if(parser.getCommandName().equals("touch")){
+                System.out.println("lol6");
+            } else if(parser.getCommandName().equals("cp")){
+                System.out.println("lol7");
+            } else if(parser.getCommandName().equals("cp -r")){
+                System.out.println("lol8");
+            } else if(parser.getCommandName().equals("rm")){
+                System.out.println("lol9");
+            } else if(parser.getCommandName().equals("cat")){
+                System.out.println("lol10");
+            } else if(parser.getCommandName().equals("wc")){
+                System.out.println("lol11");
+            } else if(parser.getCommandName().equals(">")){
+                System.out.println("lol12");
+            } else if(parser.getCommandName().equals(">>")){
+                System.out.println("lol13");
+            } else if(parser.getCommandName().equals("zip")){
+                System.out.println("lol14");
+            } else if(parser.getCommandName().equals("unzip")){
+                System.out.println("lol15");
+            } else if(parser.getCommandName().equals("exit")){
+                break;
+            }
+            else {
+                System.out.println("Wrong command. Please try again.");
+            }
+            System.out.println(parser.getCommandName());
+            for (int i = 0; i < parser.getArgs().length; i++){
+                if (parser.getArgs()[i] == null){
+                    break;
+                }
+                System.out.println(parser.getArgs()[i]);
+            }
+        }
+    }
 }
+
+
+
+
+//         Parser parser = new Parser();
+////        parser.parse("cp -r sagliaejgilahg etiahegkehus");
+////        System.out.println(parser.getCommandName());
+////        for (int i = 0; i < parser.getArgs().length; i++){
+////            if (parser.getArgs()[i] == null){
+////                break;
+////            }
+////            System.out.println(parser.getArgs()[i]);
+////        }
