b8fe1a4a708acd8f34825cb1cd0ab230e52c4866
hvmdy808 <hamdymody901@gmail.com>
2025-10-15 17:45:47 +0300

    Better parsing

--- src/Parser.java
+++ src/Parser.java
@@ -1,27 +1,36 @@
-import java.util.Arrays;
-
 public class Parser {
     private String commandName;
     private String[] args;
-    public void parse(String input) {
-        input = input.trim();
-        if (input.isEmpty()) {
+    public boolean parse(String input) {
+        try{
+            input = input.trim();
+            if (input.isEmpty()) {
+                commandName = "";
+                args = new String[0];
+                return false;
+            }
+            String [] arr = input.split(" +");
+            int i = 1;
+            commandName = arr[0];
+            if (commandName.equals("cp") && arr.length > 1 && arr[1].equals("-r")) {
+                commandName = commandName + " " + arr[1];
+                i = 2;
+            }
+            if (commandName.equals("cp -r") && arr.length <= 2) {
+                return false;
+            }
+            int j = 0;
+            args = new String[arr.length - i];
+            for (; i < arr.length; i++) {
+                args[j] = arr[i];
+                j++;
+            }
+            return true;
+        }
+        catch (Exception e) {
             commandName = "";
             args = new String[0];
-            return;
-        }
-        String [] arr = input.split(" +");
-        int i = 1;
-        commandName = arr[0];
-        if (commandName.equals("cp") && arr.length > 1 && arr[1].equals("-r")) {
-            commandName+=' '+arr[1];
-            i = 2;
-        }
-        int j = 0;
-        args = new String[arr.length - i];
-        for (; i < arr.length; i++) {
-            args[j] = arr[i];
-            j++;
+            return false;
         }
     }
     public String getCommandName() {
